e729a7e94c841c82d1e2d820b96a93861839360b
Abir-Hossain <abir.hossain@brainstation-23.com>
2023-11-04 05:04:15 +0600

    Removes unnecessary files

--- src/main/java/com/sagitta/authservice/authentication/AuthenticationController.java
+++ src/main/java/com/sagitta/authservice/authentication/AuthenticationController.java
@@ -3,34 +3,22 @@ package com.sagitta.authservice.authentication;
 import com.sagitta.authservice.authentication.domain.dto.AccountLoginRequestDto;
 import com.sagitta.authservice.authentication.domain.dto.AccountLogoutRequestDto;
 import com.sagitta.authservice.authentication.domain.dto.AccountSignupRequestDto;
-import com.sagitta.authservice.authentication.domain.dto.UserPrincipalDto;
-import com.sagitta.authservice.authentication.service.external.UserManagementService;
-
 import com.sagitta.authservice.authentication.service.AuthenticationService;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-
-import static org.springframework.http.HttpHeaders.SET_COOKIE;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/authenticate")
 public class AuthenticationController {
 
 
-    private final UserManagementService userManagementService;
-
     private final AuthenticationService authenticationService;
 
-    public AuthenticationController(UserManagementService userManagementService,
-                                    AuthenticationService authenticationService) {
-        this.userManagementService = userManagementService;
+    public AuthenticationController(
+            AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
